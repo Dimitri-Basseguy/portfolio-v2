6a5d2dd5a358df0b522bbce36d7db912adad672a
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2021-02-15 23:00:15 +0100

    dimitri datas

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 var positions = [
-    { id: "full", male: "Développeur fullstack", female: "Développeuse fullstack"},
-    { id: "front",male: "Développeur front-end", female: "Développeuse front-end"},
-    { id: "back",male: "Développeur back-end", female: "Développeuse back-end"},
-    { id: "embedded",male: "Développeur embarqué", female: "Développeuse embarqué"}
+    { id: "full", male: "Développeur fullstack", female: "Développeuse fullstack" },
+    { id: "front", male: "Développeur front-end", female: "Développeuse front-end" },
+    { id: "back", male: "Développeur back-end", female: "Développeuse back-end" },
+    { id: "embedded", male: "Développeur embarqué", female: "Développeuse embarqué" }
 ];
 
 var companies = [
@@ -14,28 +14,28 @@ var companies = [
     "Qwant"
 ]
 
-var specialities =  {
+var specialities = {
     full: ["Angular/Symphony"],
     front: ["React/Tailwind", "Vue/Material", "Angular/Bootstrap"],
     back: ["NodeJS/Express", "Python/Django"],
     embedded: ["Arduino/C++"]
 }
 
-function init(){
+function init() {
     fetch('https://randomuser.me/api/?nat=fr')
-    .then(response => response.json())
-    .then(data => {
-        var user = data.results[0];
-        user.position = positions[Math.floor(Math.random() * (positions.length))];
-        user.company = companies[Math.floor(Math.random() * (companies.length * 2))];
-        user.speciality = specialities[user.position.id][Math.floor(Math.random() * (specialities[user.position.id].length))];
-        console.log(user);
-         renderFromUser(user);
-    });
+        .then(response => response.json())
+        .then(data => {
+            var user = data.results[0];
+            user.position = positions[Math.floor(Math.random() * (positions.length))];
+            user.company = companies[Math.floor(Math.random() * (companies.length * 2))];
+            user.speciality = specialities[user.position.id][Math.floor(Math.random() * (specialities[user.position.id].length))];
+            console.log(user);
+            renderFromUser(user);
+        });
 }
 
 function renderFromUser(user) {
-    document.querySelector("#person-picture").src = user.picture.large; 
+    document.querySelector("#person-picture").src = user.picture.large;
     document.querySelector("#person-fullname").innerHTML = user.name.first + " " + user.name.last;
     document.querySelector("#person-firstname").innerHTML = user.name.first;
     document.querySelector("#person-job").innerHTML = user.position[user.gender];
@@ -44,7 +44,7 @@ function renderFromUser(user) {
     document.querySelector("#person-city").innerHTML = user.location.city + ", " + user.location.country;
     document.querySelector("#person-speciality").innerHTML = user.speciality;
     document.querySelector("#person-speciality").innerHTML = user.speciality;
-    document.querySelector("#person-email").href = "mailto:"+user.email;
+    document.querySelector("#person-email").href = "mailto:" + user.email;
 }
 
 init();
